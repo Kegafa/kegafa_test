0eebd58dae8a2f446c27c022b86a9afbe8673ff8
Ирина <tigra_359@mail.ru>
2018-12-20 17:20:00 +0300

    Alliance

--- src/main/java/main/KegafaTest.java
+++ src/main/java/main/KegafaTest.java
@@ -9,7 +9,7 @@ public class KegafaTest {
     }
 
     private void run() {
-        Pers pers1 = new Worgen("rogue");
+        Pers pers1 = new Orc("rogue");
         System.out.println("Pers1 class: " + pers1.getClassName() + ", race: " + pers1.getRaceName() + ", front: " + pers1.getFrontName());
     }
 }
